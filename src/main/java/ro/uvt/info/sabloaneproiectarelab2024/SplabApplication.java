2485bd140382a1ab8aefc7a91dac5c511eaa5b65
andreiilles <andrei.illes03@e-uvt.ro>
2025-01-09 15:22:58 +0200

    Initial commit

---
+++ src/main/java/ro/uvt/info/sabloaneproiectarelab2024/SplabApplication.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.sabloaneproiectarelab2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplabApplication.class, args);
+		System.out.println("Hello, world!");
+	}
+}
